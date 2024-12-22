158decd291ae3bf3e9e20ae525fb259211e68277
Jinny <ggab.challenge@gmail.com>
2024-12-22 23:22:34 +0900

    두 번째 페이지 디자인 완료

---
+++ script.js
@@ -0,0 +1,23 @@
+// 첫 번째 페이지와 두 번째 페이지를 선택
+const page1 = document.getElementById('page1');
+const page2 = document.getElementById('page2');
+
+// 버튼을 서택
+const page1Button = document.getElementById('page1_button'); // 말씀 뽑기 버튼
+
+// 첫 번째 페이지 -> 두번째 페이지 전환
+page1Button.addEventListener('click', () => {
+  page1.style.display = 'none'; // 첫 번째 페이지 숨기기
+  page2.style.display = 'flex'; // 두 번째 페이지 보이기
+});
+
+setTimeout(() => {
+  const loadingBox = document.querySelector('.loading-box');
+  const resultMessage = document.getElementById('result-message');
+
+  // 로딩 박스 숨기기
+  loadingBox.style.display = 'none';
+
+  // 메시지 표시
+  resultMessage.style.display = 'block';
+}, 5000); // 5초 후 실행
