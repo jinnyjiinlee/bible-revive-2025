dc1181184e7373953cd7ae56db1e321eae21c889
Jinny <ggab.challenge@gmail.com>
2025-03-27 15:56:09 +0900

    feat: LoadingTransitionPage UI 마크업 완료

---
+++ react-version/src/pages/LoadingTransitionPage.jsx
@@ -0,0 +1,14 @@
+function LoadingTransitionPage() {
+  return (
+    <div>
+      <div>로딩 아이콘 자리</div>
+      <h2>
+        내게 꼭 맞는 말씀을
+        <br />
+        찾았습니다.
+      </h2>
+    </div>
+  );
+}
+
+export default LoadingTransitionPage;
