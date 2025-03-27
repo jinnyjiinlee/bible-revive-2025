40efa6630ea251db0430a2b6967e693444232dcb
Jinny <ggab.challenge@gmail.com>
2025-03-27 15:51:49 +0900

    feat: LoadingIntroPage UI 마크업 완료

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,13 +1,15 @@
 import StartPage from './pages/StartPage';
+import LoadingIntroPage from './pages/LoadingIntroPage';
 
 function App() {
   return (
     <div>
       {/* StartPage */}
       {/* 1) StartPage.jsx안에 컴포넌트를 만들기 */}
-      <StartPage />
+      {/* <StartPage /> */}
 
-      {/* LodingPage */}
+      {/* LoadingPage */}
+      <LoadingIntroPage />
 
       {/* ResultPage  */}
     </div>
---
+++ react-version/src/pages/LoadingIntroPage.jsx
@@ -0,0 +1,10 @@
+function LoadingIntroPage() {
+  return (
+    <div>
+      <div>로딩 아이콘 자리</div>
+      <p>내게 주신 말씀을 찾는 중입니다.</p>
+    </div>
+  );
+}
+
+export default LoadingIntroPage;
