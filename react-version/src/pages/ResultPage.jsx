10f5d0dc2555d6818683eb6cd41c9e603bcf5aa7
Jinny <ggab.challenge@gmail.com>
2025-04-03 16:41:14 +0900

    feat: ResultPage 기능 구현 (백엔드 연동 제외)

--- react-version/src/pages/ResultPage.jsx
+++ react-version/src/pages/ResultPage.jsx
@@ -1,9 +1,57 @@
 import styled from 'styled-components';
 import { CommonLayoutSize } from '../components/CommonLayout.jsx';
+import { bibleVersesList } from '../constants/bibleVersesList.js';
+import { useEffect, useState } from 'react';
+import html2canvas from 'html2canvas';
+import { Link } from 'react-router-dom';
 
 function ResultPage() {
+  const [bibleVerse, setBibleVerse] = useState(null);
+
+  // 말씀 구절 랜덤으로 나오는 것
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * bibleVersesList.length);
+    setBibleVerse(bibleVersesList[randomIndex]);
+  }, []);
+
+  const handleDownload = () => {
+    html2canvas(document.getElementById('capture')).then((canvas) => {
+      saveImg(canvas.toDataURL('image/jpg'), 'bibleVerseImage.jpeg');
+    });
+  };
+
+  const saveImg = (uri, filename) => {
+    let link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = uri;
+    link.download = filename;
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  useEffect(() => {
+    if (window.Kakao && !window.Kakao.isInitialized()) {
+      window.Kakao.init('ee1c97242a0d1348b2b752a62187b335');
+    }
+    // window.Kakao.init('ee1c97242a0d1348b2b752a62187b335');
+    // window.Kakao.isInitialized();
+  });
+
+  const handleKakaoShare = () => {
+    if (window.Kakao) {
+      window.Kakao.Share.sendCustom({
+        templateId: 118994,
+        templateArgs: {
+          title: '당신을 위한 말씀',
+          description: '뽑아보세요!',
+        },
+      });
+    }
+  };
+  // <!-- templateId: 118994, -->
+
   return (
-    <CommonLayoutSize>
+    <CommonLayoutSize id='capture'>
       <div>
         <TopMainText>당신을 위한 말씀</TopMainText>
         <TopSubText>이 말씀으로 새해를 더욱 은혜롭게 시작해보세요</TopSubText>
@@ -11,31 +59,38 @@ function ResultPage() {
 
       <VerseResultBox>
         <VerseResultBoxHead>2025YEAR</VerseResultBoxHead>
-        <p>말씀나오는 구간</p>
-        <h3>말씀 주소 나오는 구간</h3>
+
+        {bibleVerse && (
+          <BibleVersePrinting id='shareCapture'>
+            <p style={{ fontSize: '15px', lineHeight: '1.5', color: '#333333' }}>{bibleVerse.text}</p>
+            <p style={{ fontSize: '16px', color: '#35225a' }}>{bibleVerse.reference}</p>
+          </BibleVersePrinting>
+        )}
       </VerseResultBox>
 
       <ButtonWrapper>
-        <Button>
+        <Button onClick={handleKakaoShare}>
           <svg viewBox='0 0 24 24' fill='#5c3c2c' style={{ width: '18px', height: '18px', marginRight: '5px' }}>
             <path d='M12 2C6.48 2 2 6.05 2 10.8c0 3.39 2.35 6.33 5.77 7.87-.08.56-.51 3.53-.56 3.81 0 0-.01.07.03.1.04.03.1.01.1.01.13-.02 3.19-2.1 3.54-2.34.71.1 1.44.15 2.12.15 5.52 0 10-4.05 10-8.8C22 6.05 17.52 2 12 2z' />
           </svg>
           공유
         </Button>
 
-        <Button>
+        <Button onClick={handleDownload}>
           <svg viewBox='0 0 24 24' fill='#5c3c2c' style={{ width: '18px', height: '18px', marginRight: '5px' }}>
             <path d='M19 21H5c-1.1 0-2-.9-2-2V5c0-1.1.9-2 2-2h11l5 5v11c0 1.1-.9 2-2 2zM5 5v14h14v-9h-4c-.55 0-1-.45-1-1V5H5z' />
           </svg>
           저장
         </Button>
 
-        <Button>
-          <svg viewBox='0 0 24 24' fill='#5c3c2c' style={{ width: '18px', height: '18px', marginRight: '5px' }}>
-            <path d='M12 4c4.42 0 8 3.13 8 7s-3.58 7-8 7H8.41l2.29 2.29-1.41 1.41L3.58 16l5.71-5.71 1.41 1.41L8.41 14H12c3.31 0 6-2.24 6-5s-2.69-5-6-5S6 5.24 6 8H4c0-3.87 3.58-7 8-7z' />
-          </svg>
-          다시
-        </Button>
+        <Link to='/' style={{ textDecorationLine: 'none' }}>
+          <Button>
+            <svg viewBox='0 0 24 24' fill='#5c3c2c' style={{ width: '18px', height: '18px', marginRight: '5px' }}>
+              <path d='M12 4c4.42 0 8 3.13 8 7s-3.58 7-8 7H8.41l2.29 2.29-1.41 1.41L3.58 16l5.71-5.71 1.41 1.41L8.41 14H12c3.31 0 6-2.24 6-5s-2.69-5-6-5S6 5.24 6 8H4c0-3.87 3.58-7 8-7z' />
+            </svg>
+            다시
+          </Button>
+        </Link>
       </ButtonWrapper>
     </CommonLayoutSize>
   );
@@ -100,4 +155,9 @@ const ButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   gap: 12px;
+`;
+
+const BibleVersePrinting = styled.div`
+  padding: 7px 30px 0 30px;
+  word-break: keep-all;
 `;
