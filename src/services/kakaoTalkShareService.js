bd092a7e7ae094ba0391ebb24ebe24e6e28c42a1
Jinny <ggab.challenge@gmail.com>
2025-04-10 17:29:20 +0900

    fix: 카카오 공유 API 연결 버튼 URL 추가

--- src/services/kakaoTalkShareService.js
+++ src/services/kakaoTalkShareService.js
@@ -3,6 +3,7 @@ export const shareKakaoTalk = (bibleVerseId) => {
     window.Kakao.Share.sendCustom({
       templateId: 118994,
       templateArgs: {
+        MY_VERSE_BUTTON_URL: 'http://localhost:3000/',
         RECEIVED_VERSE_BUTTON_URL: `result?id=${bibleVerseId}`,
       },
     });
